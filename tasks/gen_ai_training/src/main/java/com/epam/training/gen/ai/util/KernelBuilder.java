cff516ed8c8cf5ac5aac9683986517021cb74fd0
Yevhenii_Rybalko <yevhenii_rybalko@epam.com>
2025-01-08 17:26:44 +0200

    added search plugin integrated with embedding service for RAG

--- tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/util/KernelBuilder.java
+++ tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/util/KernelBuilder.java
@@ -3,6 +3,7 @@ package com.epam.training.gen.ai.util;
 import com.azure.ai.openai.OpenAIAsyncClient;
 import com.epam.training.gen.ai.plugin.CarRentalPlugin;
 import com.epam.training.gen.ai.plugin.CurrencyConversionPlugin;
+import com.epam.training.gen.ai.plugin.SearchPlugin;
 import com.microsoft.semantickernel.Kernel;
 import com.microsoft.semantickernel.aiservices.openai.chatcompletion.OpenAIChatCompletion;
 import com.microsoft.semantickernel.plugin.KernelPlugin;
@@ -21,8 +22,10 @@ public class KernelBuilder {
 
     public static final String CURRENCY_EXCHANGE_PLUGIN_NAME = "CurrencyConversionPlugin";
     public static final String CAR_RENTAL_PLUGIN_NAME = "CarRentalPlugin";
+    public static final String SEARCH_PLUGIN_NAME = "SearchPlugin";
 
     private final OpenAIAsyncClient openAIAsyncClient;
+    private final SearchPlugin searchPlugin;
 
     @Value("${client-azureopenai-deployment-name}")
     private String defaultModel;
@@ -71,6 +74,15 @@ public class KernelBuilder {
     }
 
     /**
+     * Creates a {@link KernelPlugin} bean using a search plugin.
+     *
+     * @return an instance of {@link KernelPlugin}
+     */
+    private KernelPlugin createSearchPlugin() {
+        return KernelPluginFactory.createFromObject(searchPlugin, SEARCH_PLUGIN_NAME);
+    }
+
+    /**
      * Creates a {@link Kernel} bean to manage AI services and plugins.
      *
      * @param chatCompletionService the {@link ChatCompletionService} for handling completions
@@ -81,6 +93,7 @@ public class KernelBuilder {
                 .withAIService(ChatCompletionService.class, chatCompletionService)
                 .withPlugin(createCurrencyExchangePlugin())
                 .withPlugin(createCarRentalPlugin())
+                .withPlugin(createSearchPlugin())
                 .build();
     }
 
